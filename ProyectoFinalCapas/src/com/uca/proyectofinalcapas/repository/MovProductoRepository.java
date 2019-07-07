d399d181f80e6888b646f9346dd36ce3c213343f
user <user@DESKTOP-G5N1TMT>
2019-07-07 17:00:41 -0600

    se comentareo clase para solucionar problema de inicio

--- ProyectoFinalCapas/src/com/uca/proyectofinalcapas/repository/MovProductoRepository.java
+++ ProyectoFinalCapas/src/com/uca/proyectofinalcapas/repository/MovProductoRepository.java
@@ -11,27 +11,27 @@ import com.uca.proyectofinalcapas.domain.MovProducto;
 
 public interface MovProductoRepository extends JpaRepository<MovProducto,Integer> {
 
-	public static String FIND_ALL = "select m from MovProducto m ";
+//	public static String FIND_ALL = "select m from MovProducto m ";
 	
 //	public static String FIND_BY_PRODUCTO = "";
 	
-	@Query(FIND_ALL)
-	List<MovProducto> findAllMov();
-	
-public static String FIND_ALL_MOVPRODUCTO = "select m from Movproducto m";
-	
-	public static String COUNT_ALL_MOVPRODUCTO = "select count(m.id_mov_producto) from Movproducto m";
-	
-	public static String FIND_BY_ID = "select m from Movproducto m where m.id_mov_producto = :id_mov_producto";
-	
-	@Query(FIND_ALL_MOVPRODUCTO)
-	public List<MovProducto> findAllEntrada();
-	
-	@Query(COUNT_ALL_MOVPRODUCTO)
-	public int countAllEntrada();
-	
-	@Query(FIND_BY_ID)
-	public MovProducto findById(@Param("id_mov_producto") int id_mov_producto);
+//	@Query(FIND_ALL)
+//	List<MovProducto> findAllMov();
+//	
+//public static String FIND_ALL_MOVPRODUCTO = "select m from Movproducto m";
+//	
+//	public static String COUNT_ALL_MOVPRODUCTO = "select count(m.id_mov_producto) from Movproducto m";
+//	
+//	public static String FIND_BY_ID = "select m from Movproducto m where m.id_mov_producto = :id_mov_producto";
+//	
+//	@Query(FIND_ALL_MOVPRODUCTO)
+//	public List<MovProducto> findAllEntrada();
+//	
+//	@Query(COUNT_ALL_MOVPRODUCTO)
+//	public int countAllEntrada();
+//	
+//	@Query(FIND_BY_ID)
+//	public MovProducto findById(@Param("id_mov_producto") int id_mov_producto);
 	
 	
 	
